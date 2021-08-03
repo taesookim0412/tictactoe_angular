9b754bcd5c4431349bf274897a77c7c789fee840
Tae Soo Kim <taesoo.kim0412@gmail.com>
2021-08-02 22:24:25 -0700

    thisss

--- src/app/tictactoeNgFor/board.component.ts
+++ src/app/tictactoeNgFor/board.component.ts
@@ -1,11 +1,15 @@
 import {ChangeDetectionStrategy, Component, Input} from "@angular/core";
 import {BoardService} from "../board.service";
 
+//9: index - 1.3-2ms
+//9: item: 1.3ms-1.6
+//9: no index: 1ms
 @Component({
   selector: "board",
   template: `
     Winner: None
-    <Cell *ngFor="let item of range; index as index" [boardElement]="boardService.matrix[index]" [index]="index"></Cell>
+
+    <Cell *ngFor="let item of range" [boardElement]="boardService.matrix[item]" [index]="item"></Cell>
 <!--    <Cell *ngFor="let cell of board; index as index" [boardElement]="cell" [index]="index"></Cell>-->
 
   `,
